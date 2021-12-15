5fe38578f6e6ff57aeb358fa79d3ea8676655b06
OleksiiKuc <kucheroleksiy12@gmail.com>
2021-12-15 21:04:37 +0200

    lab-4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ihor-yavorskyi-app'`, () => {
+  it(`should have as title 'kucher-oleksiy-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ihor-yavorskyi-app');
+    expect(app.title).toEqual('kucher-oleksiy-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ihor-yavorskyi-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kucher-oleksiy-app app is running!');
   });
 });
